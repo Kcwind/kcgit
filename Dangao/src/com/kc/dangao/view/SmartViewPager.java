6c425beb59e0a46f8bc511df51f521c9d0bcc74c
Kcwind <772176005@qq.com>
2013-04-29 15:28:02 +0800

    Views

---
+++ Dangao/src/com/kc/dangao/view/SmartViewPager.java
@@ -0,0 +1,20 @@
+package com.kc.dangao.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+public class SmartViewPager extends ViewPager {
+
+	public SmartViewPager(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public SmartViewPager(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
